f6c9c845cb6c67e543c1dff7598c397543fd925b
Jonathas <jon.athasmoreira@hotmail.com>
2020-08-13 00:32:45 -0300

    all changes

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,16 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { TimeSplit } from './typings/global';
+import { tick } from './utils/time';
+import { useCssHandles } from 'vtex.css-handles';
+import { useQuery } from 'react-apollo';
+import useProduct from 'vtex.product-context/useProduct';
+import productReleaseDate from './queries/productReleaseDate.graphql';
 
-interface CountdownProps { }
+const DEFAULT_TARGET_DATE = (new Date('2020-06-25')).toISOString();
+const CSS_HANDLES = ["countdown"];
+const { product: { linkText } } = useProduct()
+const { data, loading, error } = useQuery(productReleaseDate, {
+  variables: {
+    slug: linkText
+  },
+  ssr: false
+});
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
-  return <div></div>
+interface CountdownProps {
+  targetDate: string
 }
+
+const Countdown: StorefrontFunctionComponent<CountdownProps> = () => {
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    )
+  }
+  if (error) {
+    return (
+      <div>
+        <span>Erro!</span>
+      </div>
+    )
+  }
+  if (!product) {
+    return (
+      <div>
+        <span>Não há contexto de produto</span>
+      </div>
+    )
+  }
+  
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  });
+
+  const handles = useCssHandles(CSS_HANDLES)
+
+  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime)
+
+  return (
+    <div className={`${handles.countdown} db tc`}>
+      {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+    </div>
+  )
+};
 
 Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+    targetDate: {
+      title: 'Data final',
+      description: 'Data final utilizada no contador',
+      type: 'string',
+      default: null,
+    },
+  },
 }
 
 export default Countdown
